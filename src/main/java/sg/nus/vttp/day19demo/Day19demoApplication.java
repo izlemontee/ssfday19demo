30dbdf20c3b8f433b845f8612164eedd34ea26d4
Isaac Tee <iztyj.work@gmail.com>
2023-12-07 09:19:00 +0800

    initial commit

---
+++ src/main/java/sg/nus/vttp/day19demo/Day19demoApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.vttp.day19demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day19demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day19demoApplication.class, args);
+	}
+
+}
